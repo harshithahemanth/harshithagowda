ce1f2afadc302258d072c9b80ee979a18ac2513c
harshithahemanth <42990324+harshithahemanth@users.noreply.github.com>
2018-09-19 09:19:40 +0530

    Add files via upload

--- 16dice.py
+++ 16dice.py
@@ -1,5 +1,10 @@
 #random
-a=str(input("enter r to roll the dice and q to quit"))
-i=1
-import random
-print(random.randint(r,q))
+i=0
+import random as ra
+while i<5:
+	a=str(input("enter r to roll the dice and q to quit"))
+	if(a=='r'):
+		print(ra.randint(1,6))
+	else:
+		print("end game")
+		break
