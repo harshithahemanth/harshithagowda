aa45209faaa485fcc329dc867a04a82d81408468
harshithahemanth <42990324+harshithahemanth@users.noreply.github.com>
2018-10-03 09:22:29 +0530

    Add files via upload

---
+++ stonepaper.py
@@ -0,0 +1,32 @@
+#dic
+import random
+
+p={1:'r',2:'p',3:'s'}
+
+while True:
+	yc=input("you choice: r/p/s: ")
+	cc=p[random.randint(1,3)]
+
+	print("computer gave: ", cc)
+
+	if(yc=='r' and cc=='p'):
+		print("computer wins the game")
+	elif(yc=='s' and cc=='p'):
+		print("you win the game")
+	elif(yc=='r' and cc=='s'):
+		print("you win the game")
+	elif(yc=='p' and cc=='r'):
+		print("computer wins the game")
+	elif(yc=='p' and cc=='s'):
+		print("computer wins the game")
+	elif(yc=='s' and cc=='r'):
+		print("computer wins the game")
+	elif(yc=='r' and cc=='r'):
+		print("try again")
+	elif(yc=='s' and cc=='s'):
+		print("try again")
+	elif(yc=='p' and cc=='p'):
+		print("try again")
+
+	
+
